c6b92471becf3dd74f361191f13fa25e1a8054b8
anth <phamanthony47@gmail.com>
2025-01-10 19:04:18 -0500

    macbooks progress

--- database/database.sql
+++ database/database.sql
@@ -5,9 +5,11 @@ CREATE TABLE IF NOT EXISTS products
 (
     id INT AUTO_INCREMENT NOT NULL, 
     name VARCHAR(255) NOT NULL,
+    description TEXT,
     price DECIMAL(10, 2),
     brand VARCHAR(255) NOT NULL,
     PRIMARY KEY(id));
+
 
 INSERT INTO products (name, price, brand)
 VALUES ("Madagascar Centella Light Cleaning Oil", 17.88, "SKIN 1004");
@@ -15,5 +17,6 @@ VALUES ("Madagascar Centella Light Cleaning Oil", 17.88, "SKIN 1004");
 CREATE TABLE IF NOT EXISTS routines (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    description TEXT,
     products_ids JSON
 );
